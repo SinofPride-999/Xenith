b8ef7dd5f9a4c57b7830a34df06a3e720820dee4
SinofPride-999 <jhaycodes999@gmail.com>
2026-02-18 08:07:29 +0000

    chore: header comment for setup module

--- core/setup.py
+++ core/setup.py
@@ -1,2 +1,8 @@
+#######################################
+# SETUP/CONVENIENCE MODULE
+# Provides easy imports for the main run function.
+# Simplifies external access to JhayScript's core functionality.
+#######################################
+
 # This file can be used for package setup or as a convenience import
 from main import run
